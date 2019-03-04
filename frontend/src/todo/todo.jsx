56ee2cae17cdf4160919130fdb7a44c21785d9d0
Guilherme Dias <guilhermegdf2@hotmail.com>
2019-03-04 19:27:24 -0300

    ligação entre API's GET, POST e DELETE

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,6 +13,14 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleChange(e){
@@ -22,8 +30,13 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou!'))
+            .then(resp => this.refresh())
         }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+        .then(resp => this.refresh())
+    }
 
     render() {
         return (
@@ -37,7 +50,10 @@ export default class Todo extends Component {
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
                 />
-                <TodoList />
+                <TodoList
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
