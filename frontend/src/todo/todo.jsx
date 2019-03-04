0bae7f3d74ea260eadd588c136f5dc6491ddd13f
Guilherme Dias <guilhermegdf2@hotmail.com>
2019-03-04 20:24:30 -0300

    Adicionado pesquisa

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,14 +13,22 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
 
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPedding = this.handleMarkAsPedding.bind(this)
         this.refresh()
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+        .then(resp => this.setState({...this.state, description, list: resp.data}))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     handleChange(e){
@@ -31,11 +39,21 @@ export default class Todo extends Component {
         const description = this.state.description
         axios.post(URL, { description })
             .then(resp => this.refresh())
-        }
+    }
 
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
-        .then(resp => this.refresh())
+        .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+        .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPedding(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+        .then(resp => this.refresh(this.state.description))
     }
 
     render() {
@@ -46,13 +64,17 @@ export default class Todo extends Component {
                     small = 'Cadastro' 
                 />
 
-                <TodoForm 
+                <TodoForm
+                    description={this.description}
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
+                    handleSearch={this.handleSearch}
                 />
                 <TodoList
                     list={this.state.list}
                     handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPedding={this.handleMarkAsPedding}
                 />
             </div>
         )
